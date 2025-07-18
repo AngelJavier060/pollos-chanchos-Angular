4645f6fc24b592710cc5e972dff396835fe1a891
Javier <javierangelmsn@outlook.es>
2025-07-18 18:01:01 -0500

    Actualiza apiUrl para producción

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8088',
+  apiUrl: 'http://123.45.67.89:8088',// Remplazar despues para trajarlocalmente 'http://localhost:8088',
   useMockData: false, // Establecido explícitamente como false para usar datos reales del backend
   bypassAuth: false   // Mantenemos en false para usar autenticación normal
 };
